40de20317277ffeef388bcc01ea7f9c2266ae64c
Anatoliy <akomarov@wakeapp.ru>
2019-06-16 16:46:12 +0300

    11

--- src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
@@ -5,34 +5,46 @@ public class MyFirstProgram {
     public static void main(String[] args) {
 
 
-        hello("world");
-        hello("user");
-        hello("Alexey");
-        hello("world");
+        hello("User");
+        hello("User2");
+        hello("User3");
+        hello("User4");
+        hello("User5");
+
+        double l = 5;
+
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area (l));
 
 
+        double a = 4;
+        double b = 6;
 
+        System.out.println("Площадь треугольника со сторонами  " + a + " и " + b + " = " + area (a, b));
 
     }
 
-    public static void hello (String somebody ) {
 
-        System.out.println("Hello, " + somebody + "!");
+    public static void hello(String somebody) {
 
-
-        System.out.println("площадь квадрата со стороной 5 = 25");
+        System.out.println("Hello " + somebody + " !");
 
     }
 
+    public static double area (double len) {
 
-    public static double area (double len ) {
+        return len * len;
 
-
-
-        return len  * len ;
     }
+
+        public static double area (double a, double b) {
+
+
+        return a * b;
 
 }
+}
+
+
 
 
 
